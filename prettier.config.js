2c68f4cc1f3fca43f486efa84c7385b21767d9b3
Benedikt Weyer <bw.development@pm.me>
2025-10-19 19:35:34 +0200

    fix: refactor project structure and update configurations for improved styling and functionality

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSameLine: true,
+  trailingComma: 'es5',
+
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tailwindAttributes: ['className'],
+};
